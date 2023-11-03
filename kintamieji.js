7ca24dccfef8ab93bf10ebed3cfe499a45d379bc
RokasV <vezikauskas.r@gmail.com>
2023-11-03 11:04:14 +0200

    kint

--- kintamieji.js
+++ kintamieji.js
@@ -1,3 +1,44 @@
 "use strict";
 
 console.log("kintamieji js loaded");
+
+// Primityvus kintamieji
+// let arba const- deklaruojam kintamaji. rasom TIK pirma karta sukuriant kintamaji
+let age = 48; //number kintamasis
+let pi = 3.14; // number
+let year = 2023; // number
+//pakeisti year reiksme (nebedeklaruojam)
+year = 2024;
+// string - tekstas
+let name = "James";
+let town = "London";
+let month = "Lapkritis";
+
+// Boolean kintamasis -- tik 2 reiksmes true arba false
+// isUserLoggedIn - camelCase
+// IsUserLoggedIn - PascalCase
+// is-user-logged-in - kebab-case
+// is_user_logged_in - snake-case
+
+let isRaining = false;
+let isUserLoggedIn = true;
+let isSpecial = true;
+
+// null ir undefined
+// null - yra kintamasis, bet nera reiksmes arba tiesiog nera reiksmes. naudojamas isvalyti kintamojo reiksme
+// undefined - kintamojo reiksme po deklaracijos. naudojamas suzinoti ar yra nustatyta reiksme
+let userName;
+console.log(userName);
+userName = "james123";
+console.log(userName);
+// isvalyti userName reiksme
+userName = null;
+console.log(userName);
+//susikurti po kintamaji savo
+//amziui
+//miestui
+//vardui
+
+//kintamaji kiek man bus metu po 5 metu
+
+//atspausdinti su console
