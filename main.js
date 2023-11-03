573cfb8d76bac27282920d2f9d9fd3263c02a043
RokasV <vezikauskas.r@gmail.com>
2023-11-03 10:47:07 +0200

    kintam start

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log ('hello from main.js')
+// ijungti kai kurias pagal nustatymus nutylimas klaidas
+'use strict';  //butu grieztas
+
+console.log ('hello from main.js');
+
+ let title = document.getElementById("mainTitle").innerText;
+console.log(title);
