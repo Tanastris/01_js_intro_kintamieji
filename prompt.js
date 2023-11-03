354ae2dafbdc177835837a7a93fe02c1914012d5
RokasV <vezikauskas.r@gmail.com>
2023-11-03 11:39:03 +0200

    prompts

---
+++ prompt.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("prompt.js");
+// gauti ivesties reiksme is vartotojo ir grazina reiksme string tipu
+let sk1 = prompt("iveskite pirmaji skaiciu ?");
+let sk2 = prompt("iveskite antraji skaiciu ?");
+
+// sukurti skaiciuotuva daugybai
+// let sk1 = 10;
+// let sk2 = 5;
+let daugyba = sk1 * sk2;
+console.log("sk1:", sk1);
+console.log("sk2:", sk2);
+console.log(daugyba);
